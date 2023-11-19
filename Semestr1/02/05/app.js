37cde6fb47cad63dcf7151216e212edd4d425434
Paweł Łukaszuk <pawel-github@lukaszuk.net>
2023-11-19 19:53:07 +0100

    Solutions to tasks from second exercises

---
+++ Semestr1/02/05/app.js
@@ -0,0 +1,9 @@
+// zadanie polega na zainstalowaniu globalnego pakietu 'nodemon'
+// aby zainstalować pakiet globalnie w konsoli wpisujemy polecenie: npm install -g nodemon
+// flaga '-g' oznacza instalację globalną pakietu (w tym przypadku nie musimy wywoływać polecenia npm init)
+//
+// aby uruchomić naszego 'nodemon' używamy polecenie podobnie jak 'node':
+// nodemon app.js
+// następnie zmieniamy kod z tego pliku, zapisujemy zmiany i obserwujamy jak nodemon sam ponownie uruchamia
+
+console.log('aplikacja uruchomiona za pomocą nodemon');
